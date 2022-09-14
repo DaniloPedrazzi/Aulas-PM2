04d1189f5f677f6c0b4902ba5aba2ac424f3fb28
xCUELHO <danilo.justino1@gmail.com>
2022-09-14 08:38:19 -0300

    add StyledComponents

--- exemplo1/App.js
+++ exemplo1/App.js
@@ -1,8 +1,10 @@
-import { useState } from 'react';
+import { react, useState } from 'react';
+import styled from 'styled-components/native';
 import { StatusBar } from 'expo-status-bar';
 import { Image, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import tilapia from './assets/img/tilapia.jpg';
 import robalo from './assets/img/robalo.webp';
+import baiacu from './assets/img/baiacu.jpg';
 
 export default function App() {
   // useState Img
@@ -12,55 +14,53 @@ export default function App() {
   function changeImg(){
     if(img == tilapia){
       setimg(robalo);
-    }else{
+    }
+    else if(img == robalo){
+      setimg(baiacu);
+    }
+    else if(img == baiacu){
       setimg(tilapia);
     }
   }
 
   return (
-    <View style={styles.container}>
+    <Container>
       {/* Text */}
-      <Text style={styles.textWhite}>Bem vindo a troca especializada de peixes!</Text>
+      <TextWhite>Bem vindo a troca especializada de peixes!</TextWhite>
       <br/>
       {/* Image */}
-      <Image source={img} style={styles.img}></Image>
+      <Img source={img}></Img>
       <br/>
       {/* Button */}
-      <TouchableOpacity
-        onPress={() => changeImg()}
-        style={styles.button}>
-        <Text style={styles.buttonText}>Trocar de Peixe</Text>
-      </TouchableOpacity>
-      {/* ? */}
-      <StatusBar style="auto" />
-    </View>
+      <Btn onPress={() => changeImg()}>
+        <TextBtn>Trocar de Peixe</TextBtn>
+      </Btn>
+    </Container>
   );
 }
 
-// CSS
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#1c1c1c',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  img: {
-    width: 305, 
-    height: 159, 
-    borderRadius: 15
-  },
-  textWhite: {
-    color: '#fff',
-    fontSize: 25
-  },
-  button: {
-    backgroundColor: "blue",
-    padding: 20,
-    borderRadius: 15,
-  },
-  buttonText: {
-    fontSize: 20,
-    color: '#fff',
-  }, 
-});
+// StyledComponents
+const Container = styled.View`
+  flex: 1;
+  background-color: #1c1c1c;
+  align-items: center;
+  justify-content: center;
+`;
+const TextWhite = styled.Text`
+  color: #fff;
+  font-size: 25px;
+`;
+const Img = styled.Image`
+  width: 305px;
+  height: 159px; 
+  border-radius: 15px;
+`;
+const Btn = styled.TouchableOpacity`
+  backgroundColor: blue;
+  padding: 20px;
+  border-radius: 15px;
+`;
+const TextBtn = styled.Text`
+  color: #fff;
+  font-size: 20px
+`;
